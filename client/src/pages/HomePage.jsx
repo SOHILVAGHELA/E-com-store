da94cb8f4f0c3a6c35fe0bf2d29921a8b1a378b1
SOHILVAGHELA <sohilvaghela7863@gmail.com>
2024-06-21 17:51:44 +0530

    design changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -100,61 +100,72 @@ const HomePage = () => {
 
   return (
     <Layout>
-      <div className="row mt-3">
-        <div className="col-md-3">
-          <h4 className="text-center">Filter By Category</h4>
-          <div className="d-flex flex-column">
-            {categories?.map((c) => (
-              <Checkbox
-                key={c._id}
-                onChange={(e) => handleFilter(e.target.checked, c._id)}
+      <div className="container-fluid">
+        <div className="row mt-3">
+          <div className="col filter-col">
+            <h5>Filter By Category</h5>
+            <div className="d-flex flex-column text-capitalize">
+              {categories?.map((c) => (
+                <Checkbox
+                  key={c._id}
+                  onChange={(e) => handleFilter(e.target.checked, c._id)}
+                >
+                  {c.name}
+                </Checkbox>
+              ))}
+            </div>
+            <h5 className="mt-4">Filter By Price</h5>
+            <div className="d-flex flex-column">
+              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                {Prices?.map((p) => (
+                  <div key={p._id}>
+                    <Radio value={p.array}>{p.name}</Radio>
+                  </div>
+                ))}
+              </Radio.Group>
+            </div>
+            <div className="d-flex flex-column mt-4">
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  window.location.reload();
+                }}
               >
-                {c.name}
-              </Checkbox>
-            ))}
+                Reset Filter
+              </button>
+            </div>
           </div>
-          <h4 className="text-center mt-4">Filter By Price</h4>
-          <div className="d-flex flex-column">
-            <Radio.Group onChange={(e) => setRadio(e.target.value)}>
-              {Prices?.map((p) => (
-                <div key={p._id}>
-                  <Radio value={p.array}>{p.name}</Radio>
+          <div className="col-sm">
+            <h2 className="h4">All Products</h2>
+            <div className="d-flex flex-wrap">
+              {products?.map((p) => (
+                <div
+                  key={p._id}
+                  className="card m-2"
+                  style={{ width: "16.65rem" }}
+                >
+                  <img
+                    src={`http://localhost:4000/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">
+                      {p.description.substring(0, 30)}...
+                    </p>
+                    <p className="card-text">${p.price}</p>
+                    <button className="btn btn-primary">See more</button>
+                    <button className="btn btn-secondary ms-2">
+                      ADD TO CART
+                    </button>
+                  </div>
                 </div>
               ))}
-            </Radio.Group>
-          </div>
-          <div className="d-flex flex-column">
-            <button
-              className="btn btn-danger"
-              onClick={() => {
-                window.location.reload();
-              }}
-            >
-              Reset Filter
-            </button>
-          </div>
-        </div>
-        <div className="col-md-9">
-          <h1 className="text-center">All Products</h1>
-          <div className="d-flex flex-wrap">
-            {products?.map((p) => (
-              <div key={p._id} className="card m-2" style={{ width: "18rem" }}>
-                <img
-                  src={`http://localhost:4000/api/v1/product/product-photo/${p._id}`}
-                  className="card-img-top"
-                  alt={p.name}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">
-                    {p.description.substring(0, 30)}...
-                  </p>
-                  <p className="card-text">${p.price}</p>
-                  <button className="btn btn-primary m-2">See more</button>
-                  <button className="btn btn-secondary m-2">ADD TO CART</button>
-                </div>
-              </div>
-            ))}
+              {products.length == 0 && (
+                <>No Product available for this cateogry or price</>
+              )}
+            </div>
           </div>
         </div>
       </div>
